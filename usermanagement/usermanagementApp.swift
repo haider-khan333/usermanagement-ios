325a5dc0d5137b16484e839101569997639d86ab
haider-khan333 <haider.khan4444@hotmail.com>
2023-09-06 00:37:37 +0500

    Initial Commit

---
+++ usermanagement/usermanagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  usermanagementApp.swift
+//  usermanagement
+//
+//  Created by Haider on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct usermanagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
