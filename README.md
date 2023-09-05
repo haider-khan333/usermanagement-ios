63c3299827163f99b8f6d156bef5a9a03aa837b2
haider-khan333 <haider.khan4444@hotmail.com>
2023-09-06 02:30:17 +0500

    feat: email, password and submit button added only along with stylings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # User Management (iOS development using SwiftUI)
+
+### Version(0.0), 6th Sept, 2023
+*   Username, Password fields and submit button added only
   
--- usermanagement/ContentView.swift
+++ usermanagement/ContentView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    
     var body: some View {
-        
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader{ geometry in
+            ZStack{
+                Color.black
+                
+                // MARK: - red color design
+                RoundedRectangle(cornerRadius: 30,style: .continuous)
+                    .foregroundStyle(.linearGradient(colors: [.pink,.red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .rotationEffect(.degrees(120))
+                    .offset(y:-geometry.size.height * 0.45)
+                
+                // MARK: - Welcome message
+                VStack{
+                    // Text
+                    Text("Welcome")
+                        .foregroundColor(.white)
+                        .font(.system(size: 40,weight: .bold,design: .rounded))
+                        .offset(x:geometry.size.width * 0.25,
+                                y: -geometry.size.height * 0.2)
+                    
+                    // Email text field
+                    TextField("Email",text: $email)
+                        .foregroundColor(.black)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    // Password text field
+                    SecureField("Password", text: $password)
+                        .foregroundColor(.black)
+                        .textFieldStyle(.roundedBorder)
+                     
+                    
+                    // Submit Button
+                    Button {
+                        // Action
+                    } label: {
+                        Text("Sign In")
+                            .foregroundColor(.white)
+                            .frame(width: geometry.size.width * 0.5,height: geometry.size.height * 0.05)
+                            .background(
+                                RoundedRectangle(cornerRadius: 5,style: .continuous)
+                                    .fill(.red)
+                            )
+                    }
+                    
+                    
+                }
+                
+            }.ignoresSafeArea()
         }
-        .padding()
+        
     }
 }
 
